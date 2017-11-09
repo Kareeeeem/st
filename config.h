cf3f78dad6c5ec4e790607fc68d21774e20d868f
Mohammed Kareem <kareeeeem@gmail.com>
2017-11-10 00:08:56 +0100

    font config

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Monospace:size=8:antialias=true:autohint=true";
+char font[] = "Monospace:pixelsize=10:antialias=true:autohint=true";
 int borderpx = 2;
 
 /*
